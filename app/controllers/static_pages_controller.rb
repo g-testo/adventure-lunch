91dab2b187b08f301617a15a4c808b72b5ce251f
Jordan <jordanmmanley@gmail.com>
2016-02-25 17:34:24 +0000

    show 4 past lunches on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
     
     def home
         @next_lunch = Lunch.order(date: :asc).select do |i| i.date > Date.today end.first 
+        @past_lunches = Lunch.order(date: :desc).select{|i| i.date < Date.today}.take(4)
     end 
 end 
