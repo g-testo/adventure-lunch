de44e78d4c0e2b34ffa1abcaa4704aca4e7c3aa8
Jordan <jordanmmanley@gmail.com>
2016-03-26 20:13:06 +0000

    basic menu nav

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -21,6 +21,18 @@ class LunchesController < ApplicationController
     end
   end
   
+  def past_lunches
+    @past_lunches = Lunch.order(date: :desc).select{|i| i.date < Date.today}.take(8)
+  end 
+  
+  def next_lunch
+    @next_lunch = Lunch.order(date: :asc).select do |i| i.date > Date.today end.first 
+    if @next_lunch
+      redirect_to lunch_path(@next_lunch)
+    end 
+  end
+    
+  
   private 
     def lunch_params 
       params.require(:lunch).permit(:name, :url, :date, :street, :city, :state, :zipcode)
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
         @next_lunch = Lunch.order(date: :asc).select do |i| i.date > Date.today end.first 
         @past_lunches = Lunch.order(date: :desc).select{|i| i.date < Date.today}.take(4)
     end 
+    
+    def contact
+    end 
 end 
